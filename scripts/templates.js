ffe470a6ef66d5b20d2db825cf9a87b142a7731b
robinmollah <robinsajin@gmail.com>
2019-08-03 12:48:50 +0600

    AskingInflater | Condition Minimized in template inflating

--- scripts/templates.js
+++ scripts/templates.js
@@ -4,25 +4,32 @@ let debug = require('debug')('bracubot:server');
 let templates = [];
 templates["teacher"] = "%name%. Initial: %initial%. Room number: %room_number%. Email address: %mail_id%";
 templates["greetings"] = [":D", "Hi", "Hello :D", "Heyyyy ^_^"];
+templates["mail_id"] = "Here is the mail address of %name% sir: %mail_id%";
 
 
 function getTemplate(asking){
     if(util.isArray(templates[asking])){
-        //TODO take random
-        return templates[asking][0];
+        let take = Math.floor(Math.random() * templates[asking].length);
+        return templates[asking][take];
     } else {
         return templates[asking];
     }
 };
 
-
-module.exports.populate = function (tagged, reply) {
-    debug('populating');
-    if(tagged.asking == 'note'){
-        var notes = require('./db/notes');
+let askingInflater = {
+    "note": function(tagged, reply){
+        let notes = require('./db/notes');
         function callback(template, err){
             reply(template);
         };
         notes.getTemplate(tagged.params.value, callback);
-    }
+    },
+    "greetings": function(tagged, reply){
+        reply(getTemplate(tagged.asking));
+    },
+};
+
+module.exports.populate = function (tagged, reply) {
+    debug('populating');
+    askingInflater[tagged.asking](tagged,reply);
 };
