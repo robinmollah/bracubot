99e7e6bc15c8a2786b28564ad775e830d2fcd2ba
robinmollah <robinsajin@gmail.com>
2019-08-03 15:34:55 +0600

    AskingInflater architecture changed | Asking Object | Uniformed query and template to reduce error

--- scripts/db/notes.js
+++ scripts/db/notes.js
@@ -1,6 +1,14 @@
 let DB = require('./conn');
 let debug= require('debug')('bracubot:server');
-let template = 'Here is the notes of %course_code% : %course_name% course: %link%';
+let template = {
+    asking: {
+        'note': {
+            out : 'Here is the notes of %course_code% : %course_name% course: %link%',
+            query: 'SELECT notes.course_code, notes.link, courses.course_name FROM notes INNER JOIN courses ON ' +
+                'courses.course_code = notes.course_code WHERE notes.course_code = ? AND verified = 1',
+        },
+    },
+};
 let notes = {
     getCourseLink: function(course_code, callback){
         debug("querying for: " + course_code);
@@ -11,10 +19,16 @@ let notes = {
     getTemplate: function(course_code, reply){
         this.getCourseLink(course_code, populator);
         function populator(data, err) {
-            debug(data);
-            template = template.replace("%course_code%", data[0].course_code);
-            template = template.replace("%link%", data[0].link);
-            template = template.replace("%course_name%", data[0].course_name);
+            console.log(data);
+            var property;
+            data = data[0];
+            for(property in data){
+                var str = template;
+                template = template.replace("%" + property +"%", data[property]);
+                if(str == template){
+                    console.error("There is a problem in template replacing");
+                }
+            }
             reply(template);
         };
     },
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,11 +1,24 @@
 let util = require('util');
 let debug = require('debug')('bracubot:server');
+let DB = require('./db/conn');
 
 let templates = [];
 templates["teacher"] = "%name%. Initial: %initial%. Room number: %room_number%. Email address: %mail_id%";
 templates["greetings"] = [":D", "Hi", "Hello :D", "Heyyyy ^_^"];
 templates["mail_id"] = "Here is the mail address of %name% sir: %mail_id%";
 
+let temple = {
+    asking: {
+        'note': {
+            out : 'Here is the notes of %course_code% : %course_name% course: %link%',
+            query: 'SELECT notes.course_code, notes.link, courses.course_name FROM notes INNER JOIN courses ON ' +
+                'courses.course_code = notes.course_code WHERE notes.course_code = ? AND verified = 1',
+        },
+        'greetings': {
+            out: ':D',
+        }
+    },
+};
 
 function getTemplate(asking){
     if(util.isArray(templates[asking])){
@@ -16,20 +29,31 @@ function getTemplate(asking){
     }
 };
 
-let askingInflater = {
-    "note": function(tagged, reply){
-        let notes = require('./db/notes');
-        function callback(template, err){
-            reply(template);
+let askingInflater = function(tagged, reply){
+        function callback(data, err){
+            console.log(data);
+            if(!data) throw err;
+            data = data[0];
+            var property;
+            var str = temple.asking[tagged.asking].out;
+            let res = str;
+            for(property in data){
+                var prevStr = str;
+                res = res.replace("%" + property +"%", data[property]);
+                if(prevStr == res) console.error('Something wrong while replacing.');
+            }
+            reply(res);
         };
-        notes.getTemplate(tagged.params.value, callback);
-    },
-    "greetings": function(tagged, reply){
-        reply(getTemplate(tagged.asking));
-    },
+        // notes.getTemplate(tagged.params.value, callback);
+        let asking = temple.asking[tagged.asking];
+        if(asking.query){
+            DB.query(asking.query, tagged.params.value, callback);
+        } else {
+            reply(asking.out);
+        }
 };
 
 module.exports.populate = function (tagged, reply) {
     debug('populating');
-    askingInflater[tagged.asking](tagged,reply);
+    askingInflater(tagged,reply);
 };
