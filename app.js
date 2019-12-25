81e255f74fa4fff0fddc31d9dc81fbe7bf7429a3
robinmollah <robinsajin@gmail.com>
2019-12-25 16:02:05 +0600

    Facebook Chat Plugin added

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ let fbapi = require('./scripts/fbapi');
 app.use(bodyParser.json());
 app.use(webhooks);
 app.use(teachers);
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/test',function(req,res) {
     res.sendFile(__dirname + '/test.html');
 });
+
 app.get('/',function(req, res){
-    res.render('index',{page : 'Hi', menuId: 4});
+    res.render('index.pug',{page : 'Hi', menuId: 4});
 });
 app.get('/privacy_policy',function(req,res){
     res.sendFile(__dirname + '/views/privacy_policy.html');
